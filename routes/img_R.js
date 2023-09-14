158e70605a6b6ca5c637ec294da8220010c719fc
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-14 10:51:57 +0300

    running to_do_list project with file uploading

--- routes/img_R.js
+++ routes/img_R.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
         cb(null,'images/'); // Uploads will be stored in the 'uploads' directory
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname); // Generate a unique filename
+        cb(null,file.originalname); // Generate a unique filename
     },
 });
 
@@ -15,8 +15,15 @@ const upload = multer({ storage: storage });
 
 
 router.post('/upload', upload.single('file'), (req, res) => {
-        if (!req.file) {
-            return res.status(400).send('No file uploaded.');
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    let name = '/' + req.file.destination+ req.file.filename;
+    let q = `INSERT INTO files_tbl(file_name) VALUES('${name}')`;
+    db_pool.query(q,function (err,rows,fields){
+        if (err){
+            res.status(500).json({message:err});
         }
-        res.send('File uploaded successfully.');
+    })
+    res.redirect("/front/bg");
 });
